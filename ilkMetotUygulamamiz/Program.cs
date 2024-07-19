f3b392e12ee53ee1f443cb9b6f4aab57b40b3f1b
Burak Kırıcı <burakpinar76@gmail.com>
2024-07-19 21:28:55 +0300

    commit

--- ilkMetotUygulamamiz/Program.cs
+++ ilkMetotUygulamamiz/Program.cs
@@ -1,14 +1,19 @@
-﻿// Geriye değer döndürmeyen bir void metot
+﻿
+{
+    sarkiSozu();
+    System.Console.WriteLine($"2 ye Bolumunden kalan {rastgeleSayi()}");
+    Console.WriteLine($"Carpma isleminin sonucu: {carpimUyg(5,7)}");
+    isimSoyisim("burak", "kırıcı");
+}
+
+
+
 void sarkiSozu()
 {
     Console.WriteLine("Eskisi gibi değiliz");
 }
-sarkiSozu();
 // Geriye tamsayı Döndüren Bir Metot.
-{
-    int kalan = rastgeleSayi();
-    Console.WriteLine($"Rastgele sayinin 2 ye bölümünden kalan: {kalan}");
-}
+
 int rastgeleSayi()
 {
 Random rnd = new Random();
@@ -18,19 +23,12 @@ return rastgele % 2;
 
 // Parametre Alan ve Geriye Değer Döndüren Bir Metot
 
-int carpimUyg(int x=5, int y=4)
+int carpimUyg(int x, int y)
 {
     return x*y;
 }
-carpimUyg();
 // Parametre Alan ve Geriye Değer Döndürmeyen Bir Metot
-static void Main(string[] args)
-{
-    string name = "Burak";
-    string surname = "Kırıcı";
 
-    isimSoyisim(name, surname);
-}
 static void isimSoyisim(string name, string surname)
 {
     
--- ilkMetotUygulamamiz/obj/Debug/net8.0/ilkMetotUygulamamiz.AssemblyInfo.cs
+++ ilkMetotUygulamamiz/obj/Debug/net8.0/ilkMetotUygulamamiz.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ilkMetotUygulamamiz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84d41615009783ec1a3a1d5470169372d8906a8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ilkMetotUygulamamiz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ilkMetotUygulamamiz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
